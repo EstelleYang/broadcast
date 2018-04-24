c009ed277f7d660ef717774b79c04febefba0e8d
yangjing <yangjingclass@163.com>
2018-04-24 16:35:39 +0800

    jk

--- app/src/main/java/com/examplelist/study/broadcastpractice/MainActivity.java
+++ app/src/main/java/com/examplelist/study/broadcastpractice/MainActivity.java
@@ -1,6 +1,12 @@
 package com.examplelist.study.broadcastpractice;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends BaseActivity {
 
@@ -23,6 +30,49 @@ public class MainActivity extends BaseActivity {
                 sendBroadcast(intent);
             }
         });
+        final Button call = (Button)findViewById(R.id.make_call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*try{
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);}
+                catch (SecurityException e){
+                    e.printStackTrace();
+                }*/
+                /**
+                 * 先判断用户授权没有，如果授权，执行   如果还未授权，询问用户
+                 */
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},1);
+                }else{
+                    call();
+                }
+            }
+        });
+    }
+    private void call(){
+        try{
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    call();
+                }else{
+                    Toast.makeText(this,"拒绝访问",Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+        }
+    }
 }
